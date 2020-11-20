233898a392d32a6854ea28fb25cbfeaf459f71db
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-11-20 10:25:09 -0600

    projects component

---
+++ src/components/data/projects_data.js
@@ -0,0 +1,62 @@
+import portfolio from '../../assets/projects_images/portfolio.jpg'
+
+const data_projects = [
+  {
+    name: 'Dev Portfolio 1',
+    image: portfolio,
+    deployed_url: '',
+    github_url: '',
+    category: ['vanilla'],
+  },
+  {
+    name: 'Dev Portfolio 2',
+    image: portfolio,
+    deployed_url: '',
+    github_url: '',
+    category: [''],
+  },
+  {
+    name: 'Dev Portfolio 3',
+    image: portfolio,
+    deployed_url: '',
+    category: ['vanilla','react.js'],
+  },
+  {
+    name: 'Dev Portfolio 4',
+    image: portfolio,
+    deployed_url: '',
+    category: ['html_css'],
+  },
+  {
+    name: 'Dev Portfolio 5',
+    image: portfolio,
+    deployed_url: '',
+    category: ['vanilla'],
+  },
+  {
+    name: 'Dev Portfolio 6',
+    image: portfolio,
+    deployed_url: '',
+    category: ['node.js','mongoDB'],
+  },
+  {
+    name: 'Dev Portfolio 7',
+    image: portfolio,
+    deployed_url: '',
+    category: ['vanilla'],
+  },
+  {
+    name: 'Dev Portfolio 8',
+    image: portfolio,
+    deployed_url: '',
+    category: ['vanilla'],
+  },
+  {
+    name: 'Dev Portfolio 9',
+    image: portfolio,
+    deployed_url: '',
+    category: [],
+  },
+]
+
+export default data_projects
