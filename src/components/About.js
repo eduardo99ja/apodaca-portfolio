f79195d01ed9f5f7a4f9cfa2531a8d768e2c07f5
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-11-19 19:56:40 -0600

    about component design

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import api from '../assets/icons/api.svg'
+import SkillCard from './SkillCard'
 
+const skills = [
+  {
+    icon: api,
+    title: 'lorem ipsum dolor sit.',
+    about:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates mollitia',
+  },
+  {
+    icon: api,
+    title: 'lorem ipsum dolor sit.',
+    about:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates mollitia',
+  },
+  {
+    icon: api,
+    title: 'lorem ipsum dolor sit.',
+    about:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates mollitia',
+  },
+  {
+    icon: api,
+    title: 'lorem ipsum dolor sit.',
+    about:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates mollitia',
+  },
+]
 const About = () => {
-    return (
-        <div>
-            About me
+  return (
+    <div className='about'>
+      <h6 className='about__intro'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+        mollitia optio architecto, obcaecati excepturi odit asperiores atque
+        quod sed minima aperiam fugit unde, harum recusandae exercitationem.
+        Eligendi hic tempora esse?
+      </h6>
+      <div className='container about__container'>
+        <h6 className='about__heading'>What I offer</h6>
+        <div className='row'>
+          {skills.map(skill => (
+            <SkillCard skill={skill} />
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default About
